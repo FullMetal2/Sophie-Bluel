1e63e295ae0dd5d616f2694712a4954d3bad649e
Jb <jean.baptiste.bertron@gmail.com>
2025-05-29 15:05:18 +0200

    Nouveau départ : dépôt réinitialisé

---
+++ FrontEnd/travaux.js
@@ -0,0 +1,135 @@
+            ///// Récupération des travaux depuis l'API /////
+async function getWorks() {
+
+        const response = await fetch("http://localhost:5678/api/works"); // Serveur API //
+            console.log("Réponse brute de l'API :", response);
+        const works = await response.json();
+            console.log("Transformation de la réponse API en tableau utilisable en JS", works);  // works = tableau de données (Works) //
+
+
+
+            ///// Fonction pour trier par categorie /////
+            
+function trierCategory(works){
+        const worksCategory = {};
+
+            // Boucle for qui parcours le tableau works //
+
+            for (let i = 0; i < works.length; i++) {
+                const categoryId = works[i].category.id;
+                console.log(categoryId);
+
+                    if (!worksCategory[categoryId]) {
+                        worksCategory[categoryId] = {
+                            name: works[i].category.name,
+                            item: []
+                        };
+                    };
+                worksCategory[categoryId].item.push(works[i]);
+            };
+                console.log(worksCategory);
+}
+
+
+            
+
+
+                //Création des boutons filtres ( balises du DOM ) //
+        const btnFiltre = document.querySelector(".btn-filtre")
+        let btnTous = document.querySelector(".btn-filtre");
+
+        btnTous = document.createElement("button");
+        btnTous.textContent = "Tous";
+        btnTous.addEventListener("click", function() {
+        
+        })
+
+        //const btnFiltre = document.querySelector(".btn-filtre")
+        let btnObjet = document.querySelector(".btn-filtre");
+
+        btnObjet = document.createElement("button");
+        btnObjet.textContent = "Objets";
+        btnObjet.addEventListener("click", function() {
+
+        })
+        
+
+        //const btnFiltre = document.querySelector(".btn-filtre")
+        let btnAppart = document.querySelector(".btn-filtre");
+
+        btnAppart = document.createElement("button");
+        btnAppart.textContent = "Appartements";
+        btnAppart.addEventListener("click", function() {
+
+        })
+        
+
+        //const btnFiltre = document.querySelector(".btn-filtre")
+        let btnHotel = document.querySelector(".btn-filtre");
+
+        btnHotel = document.createElement("button");
+        btnHotel.textContent = "Hôtels & Restaurants";
+        btnHotel.addEventListener("click", function() {
+
+        })
+
+        btnFiltre.appendChild(btnTous);
+        btnFiltre.appendChild(btnObjet);
+        btnFiltre.appendChild(btnAppart);
+        btnFiltre.appendChild(btnHotel);
+
+        
+
+                    // Apelle à la fonction genereWorks //
+                    
+    genereWorks(works);
+    trierCategory(works);
+    
+    
+}
+           
+            // Apelle à la fonction getWorks //
+    getWorks();
+
+
+ 
+            // Boucle for pour afficher les travaux //
+
+function genereWorks(works){
+    for (let i = 0; i < works.length; i++) {
+
+            // Création des balises HTML //
+
+        const figure = works[i];
+
+            // Rattachement au balise du DOM //
+
+        const divGallery = document.querySelector(".gallery");
+
+        const worksElement = document.createElement("figure");
+        worksElement.id = works[i].id;
+
+        const worksImg = document.createElement("img");
+        worksImg.src = figure.imageUrl;
+
+        const worksTitle = document.createElement("figcaption");
+        worksTitle.innerText = figure.title;
+
+            // Rattachement à la section du DOM //
+
+        divGallery.appendChild(worksElement);
+        worksElement.appendChild(worksImg);
+        worksElement.appendChild(worksTitle);
+    }
+}
+
+
+
+
+
+
+
+
+
+            
+

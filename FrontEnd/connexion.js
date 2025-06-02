56655224157d59e88f9dfb0ef437a1b5f863d25a
Jb <jean.baptiste.bertron@gmail.com>
2025-06-02 17:17:27 +0200

    style formulaire

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -7,23 +7,23 @@
             const elementMain = document.querySelector('main')
             elementMain.innerHTML="";
             const elementForm = document.querySelector(".logins");
-            elementForm.style.display = "block";
+            elementForm.classList.add("visible-form");
         })
 
         
         loginForm = document.querySelector(".logins")
             loginForm.innerHTML = `
                 <h2>Log In</h2>
-                    <form action"submit" method="POST" id="form-login">
+                    <form action"#" method="POST" id="form-login">
                         <div>
                             <label for="email">E-mail</label>
-                            <input type="email" id="email" required>
+                            <input type="email" id="email" name="email" required>
                             <label for="password">Mot de passe</label>
-                            <input type="password" id="password required>
+                            <input type="password" id="password" name="password" required>
                         </div>
                         <div>
                         <input type="submit" value="Se connecter"/>
                         </div>
                     </form>`;
 
-        loginForm.appendChild(loginForm);
+        logins.appendChild(loginForm);

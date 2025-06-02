3635d6be6dcc5519b309c0fde24e1393495b60ea
Jb <jean.baptiste.bertron@gmail.com>
2025-06-02 15:57:56 +0200

    formulaire de connexion

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -3,7 +3,27 @@
     
     const btnLogin = document.querySelector("#login");
 
-        btnLogin.addeventlistener("click", () => {
+        btnLogin.addEventListener("click", () => {
             const elementMain = document.querySelector('main')
-            elementMain.style.display = 'none'
+            elementMain.innerHTML="";
+            const elementForm = document.querySelector(".logins");
+            elementForm.style.display = "block";
         })
+
+        
+        loginForm = document.querySelector(".logins")
+            loginForm.innerHTML = `
+                <h2>Log In</h2>
+                    <form action"submit" method="POST" id="form-login">
+                        <div>
+                            <label for="email">E-mail</label>
+                            <input type="email" id="email" required>
+                            <label for="password">Mot de passe</label>
+                            <input type="password" id="password required>
+                        </div>
+                        <div>
+                        <input type="submit" value="Se connecter"/>
+                        </div>
+                    </form>`;
+
+        loginForm.appendChild(loginForm);
